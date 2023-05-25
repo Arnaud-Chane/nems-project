d922ba4ba3edef9fa33e602367b205c4d9bd819c
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-05-25 22:02:12 +0200

    Connection ok

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 
 
+
+app.get("/message", (req, res) => {
+  res.json({ message: "Hello" });
+});
 
 
 
