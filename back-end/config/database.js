ddb19a7940c9650050a105f46877f97f9b8f72a2
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-05-30 21:59:40 +0200

    Delete ok but error in console & response 23 Home not assigned

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -8,5 +8,5 @@ export const database = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT
 });
--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -5,26 +5,34 @@ import { Link } from "react-router-dom";
 function Home() {
   const [movies, setMovies] = useState([]);
 
-  const API_URL = "http://localhost:7000/api/movies";
+  // const API_URL = "http://localhost:7000/api/movies";
+  const client = axios.create({
+    baseURL: "http://localhost:7000/api/movies"
+  })
+
+  const getAllMovies = async () => {
+    const response = await client.get('');
+    setMovies(response.data);
+  }
 
   useEffect(() => {
-    async function getAllMovies() {
-      try {
-        const { data } = await axios.get(API_URL);
-        setMovies(data);
-      } catch (err) {
-        console.log(err);
-      }
-    }
     getAllMovies();
   }, []);
+
+  const handleClick = (id) => {
+    const response = client.delete(`${id}`);
+    setMovies(movies.filter((movie) => movie.id != id))
+  }
 
   return (
     <div>
       {movies.map((movie) => (
-        <Link to={`/movies/${movie.id}`} key={movie.id}>
-          <li>{movie.title}</li>
-        </Link>
+        <div className="movies-div" key={movie.id}>
+          <Link to={`/movies/${movie.id}`} >
+            <li>{movie.title}</li>
+          </Link>
+          <button className="btn-delete" type="button" onClick={() => handleClick(movie.id)}>X</button>
+        </div>
       ))}
     </div>
   );
