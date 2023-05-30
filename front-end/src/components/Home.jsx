f0c8d7c6d6f0895f40e746c075f9b1af94c47601
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-05-30 11:21:35 +0200

    Change classic axios to axios from FreeCodeCamp

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -5,19 +5,29 @@ import { Link } from "react-router-dom";
 function Home() {
   const [movies, setMovies] = useState([]);
 
-  const API_URL = "http://localhost:7000/api/movies";
+  // const API_URL = "http://localhost:7000/api/movies";
+  const client = axios.create({
+    baseURL: "http://localhost:7000/api/movies"
+  });
+
+  // useEffect(() => {
+  //   async function getAllMovies() {
+  //     try {
+  //       const { data } = await axios.get('');
+  //       setMovies(data);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   }
+  //   getAllMovies();
+  // }, []);
 
   useEffect(() => {
-    async function getAllMovies() {
-      try {
-        const { data } = await axios.get(API_URL);
-        setMovies(data);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    getAllMovies();
-  }, []);
+    client.get('')
+    .then((response) => {
+      setMovies(response.data)
+    })
+  });
 
   return (
     <div>
